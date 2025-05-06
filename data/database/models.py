73b1351b705c7cd1c9a7a59205b7f10ae01edb65
mkornieieva <miloslavakorneeva@gmail.com>
2025-05-06 18:53:17 +0200

    last model

--- data/database/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, LargeBinary
-from database import Base
-
-
-class ProcessedImage(Base):
-    __tablename__ = "ProcessedImages"
-
-    proc_image_id = Column(Integer, primary_key=True, index=True)
-    image_data = Column(LargeBinary, nullable=False)
--- data/database/services.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy.orm import Session
-from models import ProcessedImage
-
-def get_image_by_id(db: Session, image_id: int):
-    return db.query(ProcessedImage).filter(ProcessedImage.proc_image_id == image_id).first()
